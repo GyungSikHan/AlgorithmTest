9225dfa7d319cfb6c578b88b70078c030b2f34e0
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2024-12-27 14:18:48 +0900

    [Silver I] Title: 안전 영역, Time: 20 ms, Memory: 2420 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -1,70 +1,64 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 
 using namespace std;
-
-int dY[4] = {-1,0,1,0};
-int dX[4] = {0,1,0,-1};
+int dy[4] = { 0,-1,0,1 };
+int dx[4] = { -1,0,1,0 };
 int n;
-int arr[105][105];
-bool visited[105][105];
-int MaxData;
-int MaxCount;
+int arr[101][101];
+bool visited[101][101];
+int Maxdata;
+int Maxcount;
 
-void DFS(int y, int x, int k)
+void DFS(int data, int y, int x)
 {
-    visited[y][x] = true;
-    for (int i = 0; i < 4; i++)
-    {
-        int Y = y + dY[i];
-        int X = x + dX[i];
-        if(Y >= 0 && Y < n && X >= 0 && X < n)
-        {
-            if(arr[Y][X] > k && visited[Y][X] == false)
-                DFS(Y,X ,k);
-        }
-    }
-    
+	visited[y][x] = true;
+	for (int i = 0; i < 4; i++)
+	{
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+
+		if(ny >= 0 && ny < n && nx >= 0 && nx <n)
+		{
+			if(visited[ny][nx] == false && arr[ny][nx] > data)
+			{
+				DFS(data, ny, nx);
+			}
+		}
+	}
 }
 
 int main()
 {
-    cin>>n;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            {
-                int temp;
-                cin >>temp;
-                arr[i][j] = temp;
-                MaxData = max(MaxData, temp);
-            }
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			int temp{};
+			cin >> temp;
+			arr[i][j] = temp;
+			Maxdata = max(Maxdata, temp);
+		}
+	}
+	int temp{};
+	for (int k = 0; k <= Maxdata; k++)
+	{
+		Maxcount = 0;
+		fill(&visited[0][0], &visited[0][0] + 101 * 101,0);
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (visited[i][j] == false && arr[i][j] > k)
+				{
+					Maxcount++;
+					DFS(k, i, j);
+				}
+			}
+		}
+		temp = max(Maxcount, temp);
+	}
 
-    for (int k = 0; k < MaxData; k++)
-    {
-        int count{};
-        fill(&visited[0][0], &visited[0][0] + 105*105, false);
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if(arr[i][j] <= k)
-                    visited[i][j] = true;
-            }
-        }
-
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if(visited[i][j] == false &&arr[i][j] > k)
-                {
-                    count++;
-                    DFS(i,j, k);
-                }
-            }
-        }
-
-        MaxCount = max(count , MaxCount);
-    }
-    
-    cout<<MaxCount<<endl;
+	cout << temp << endl;
 }
