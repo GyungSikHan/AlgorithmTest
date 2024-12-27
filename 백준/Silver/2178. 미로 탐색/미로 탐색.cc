908693fc99999c66610071a45d6a94f2d91807fb
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2024-12-27 11:57:47 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,52 +1,52 @@
 #include <iostream>
-#include<queue>
+#include <vector>
+#include <queue>
 using namespace std;
 
-int y[4] = {-1,0,1,0};
-int x[4] = {0,1,0,-1};
-
-int n,m;
-int bard[104][104];
-
-int visited[104][104];
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,-1,0,1 };
+int n, m;
 queue<pair<int, int>> q;
+vector<vector<int>> v;
+vector<vector<int>> visited;
 
-void BFS(int nY, int nX)
+void DFS(int y, int x)
 {
-    q.push({nY, nX});
-    visited[nY][nX] = 1;
+	q.push({y, x});
+	visited[y][x] = 1;
 
-    while (q.empty() == false)
-    {
-        int currY = q.front().first;
-        int currX = q.front().second;
-        q.pop();
-
-        for (int i = 0; i < 4; i++)
-        {
-            int X = currX+x[i];
-            int Y = currY+y[i];
-
-            if(Y >= 1 && Y <= n && X >= 1 && X <= m)
-            {
-                if(bard[Y][X] == 1 && visited[Y][X] == 0)
-                {
-                    q.push({Y,X});
-                    visited[Y][X] = visited[currY][currX] + 1;
-                }
-            }
-        }   
-    }
-    
+	while (q.empty() == false)
+	{
+		int curry = q.front().first;
+		int currx = q.front().second;
+		q.pop();
+		
+		for (int i = 0; i < 4; i++)
+		{
+			int ny = curry + dy[i];
+			int nx = currx + dx[i];
+			if(ny >=0 && ny < n && nx >= 0 && nx < m)
+			{
+				if(visited[ny][nx] == false && v[ny][nx] == 1)
+				{
+					visited[ny][nx] = visited[curry][currx] + 1;
+					q.push({ ny,nx });
+				}
+			}
+		}
+	}
 }
 
 int main()
 {
-    cin>>n>>m;
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= m; j++)
-            scanf("%1d" ,&bard[i][j]);
-        
-    BFS(1,1);
-    cout<<visited[n][m];
+	cin >> n >> m;
+	v.resize(n, vector<int>(m, 0));
+	visited.resize(n, vector<int>(m, 0));
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			scanf("%1d", &v[i][j]);
+
+
+	DFS(0,0);
+	cout << visited[n-1][m-1] << endl;
 }
