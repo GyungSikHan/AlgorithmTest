3872c40abfe04a0f7b58944802745bd1c60c64d2
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-01-08 15:14:22 +0900

    [Gold IV] Title: 치즈, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.cc
+++ 백준/Gold/2636. 치즈/치즈.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <queue>
 #include <memory.h>
 using namespace std;
 int dx[4] = {0,-1,0,1}, dy[4] {-1,0,1,0};
@@ -8,35 +7,6 @@ int n,m;
 int times{};
 int arr[100][100];
 int visited[100][100];
-
-void BFS(int x, int y)
-{
-    queue<pair<int,int> > q;
-    q.push(make_pair(x,y));
-
-    while (q.empty() == false)
-    {
-        int currx = q.front().first;
-        int curry = q.front().second;
-        visited[currx][curry] = 1;
-        q.pop();
-
-        for (int i = 0; i < 4; i++)
-        {
-            int nx = currx+dx[i];
-            int ny = curry +dy[i];
-            if (nx < 0 || nx >= n || ny <0 || ny >=m)
-                continue;
-            if (visited[nx][ny] == 0)
-            {
-                if (arr[nx][ny] == 1)
-                    visited[nx][ny] = 2;
-                else
-                    q.push(make_pair(nx,ny));
-            }
-        }
-    }
-}
 
 void DFS(int x, int y)
 {
