f514d046a8dd6dd96b6a9ade215571ff46e29045
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-03-24 10:02:55 +0900

    Update 두 개 뽑아서 더하기.cpp
    set으로 바꿔서 풀어봄

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cpp
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cpp
@@ -1,24 +1,41 @@
 #include <string>
 #include <vector>
+#include<set>
 #include<algorithm>
 
 using namespace std;
 
+// vector<int> solution(vector<int> numbers)
+// {
+//     vector<int> answer;
+//     sort(numbers.begin(), numbers.end());
+
+//     for (int i = 0; i < numbers.size(); i++)
+//     {
+//         for (int j = i+1; j < numbers.size(); j++)
+//         {
+//             answer.push_back(numbers[i]+numbers[j]);
+//         }
+        
+//     }
+//     sort(answer.begin(), answer.end());
+//     answer.erase(unique(answer.begin(), answer.end()),answer.end());
+
+//     return answer;
+// }
+
 vector<int> solution(vector<int> numbers)
 {
-    vector<int> answer;
-    sort(numbers.begin(), numbers.end());
-
+    set<int> temp;
     for (int i = 0; i < numbers.size(); i++)
     {
         for (int j = i+1; j < numbers.size(); j++)
         {
-            answer.push_back(numbers[i]+numbers[j]);
+            temp.insert(numbers[i]+numbers[j]);
         }
         
     }
-    sort(answer.begin(), answer.end());
-    answer.erase(unique(answer.begin(), answer.end()),answer.end());
-
+    vector<int> answer(temp.begin(), temp.end());
+   
     return answer;
 }
