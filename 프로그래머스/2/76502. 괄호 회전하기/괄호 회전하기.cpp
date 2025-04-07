1d0a98480fc2f9a0001249170e4220b60dcf20df
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-04-07 20:22:16 +0900

    [level 2] Title: 괄호 회전하기, Time: 8.62 ms, Memory: 3.95 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include<stack>
+using namespace std;
+
+int solution(string s) 
+{
+    int answer = -1;
+    string temp = s;
+    int count{};
+    do
+    {
+        stack<char> st;
+        for (int i = 0; i < temp.size(); i++)
+        {
+            if(st.empty() == true)
+                st.push(temp.at(i));
+            else
+            {
+                if(st.top() == '(' && temp.at(i) == ')')
+                    st.pop();             
+                else if(st.top() == '{' && temp.at(i) == '}')
+                    st.pop();
+                else if(st.top() == '[' && temp.at(i) == ']')
+                    st.pop();
+                else
+                    st.push(temp.at(i));
+            }
+        }
+        if(st.empty() == true)
+            count++;
+        char c{};
+        for (int i = 0; i < temp.size(); i++)
+        {
+            if(i == 0)
+                c = temp.at(i);
+            else
+                temp[i-1] = temp[i];
+        }
+        temp[temp.size()-1] = c;
+        
+    }while(temp != s);
+    
+    answer = count;
+    return answer;
+}
