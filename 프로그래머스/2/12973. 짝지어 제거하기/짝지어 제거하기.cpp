38d2d575f7bee97aa2e4963c3804051ce50ad755
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-04-07 20:33:21 +0900

    [level 2] Title: 짝지어 제거하기, Time: 5.22 ms, Memory: 6.29 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<string>
+#include<stack>
+using namespace std;
+
+int solution(string s)
+{
+    int answer = 0;
+    stack<char>st;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if(st.empty() == true)
+            st.push(s.at(i));
+        else if(st.top() == s.at(i))
+            st.pop();
+        else
+            st.push(s.at(i));
+    }
+
+    if(st.empty() == true)
+        answer=1;
+    
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
+    return answer;
+}
