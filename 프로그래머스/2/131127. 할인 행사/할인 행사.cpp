f96e1f446aea121af92e8ad5ba701c91120d13c0
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-04-24 13:18:17 +0900

    [level 2] Title: 할인 행사, Time: 70.25 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -12,26 +12,34 @@ int solution(vector<string> want, vector<int> number, vector<string> discount)
     for (int i = 0; i < want.size(); i++)
         map.insert({want[i], number[i]});
     
-    for (int i = 0; i < discount.size(); i++)
+    // for (int i = 0; i < discount.size(); i++)
+    // {
+    //     unordered_map<string, int> temp = map;
+    //     if(i+10 > discount.size())
+    //         break;
+    //     for (int j = i; j < i+10; j++)
+    //     {
+    //         if(temp.find(discount[j]) != temp.end())
+    //             temp[discount[j]]--;
+    //     }
+    //     int count{};
+    //     for(pair<string, int> iter : temp)
+    //     {
+    //         if(iter.second == 0)
+    //             count++;
+    //     }
+    //     if(count == want.size())
+    //         answer++;
+    // }
+for (int i = 0; i < discount.size()-9; i++)
     {
-        unordered_map<string, int> temp = map;
-        if(i+10 > discount.size())
-            break;
+        unordered_map<string,int> temp{};
         for (int j = i; j < i+10; j++)
-        {
-            if(temp.find(discount[j]) != temp.end())
-                temp[discount[j]]--;
-        }
-        int count{};
-        for(pair<string, int> iter : temp)
-        {
-            if(iter.second == 0)
-                count++;
-        }
-        if(count == want.size())
+            temp[discount[j]]++;
+
+        if(map == temp)
             answer++;
-    }
-    
+    }    
 
     return answer;
 }
