ffeb49380590b210d226b9b6f594540ff52676d6
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-04-24 13:04:48 +0900

    [level 2] Title: 할인 행사, Time: 56.63 ms, Memory: 8.87 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include<unordered_map>
+
+using namespace std;
+
+int solution(vector<string> want, vector<int> number, vector<string> discount)
+{
+    int answer = 0;
+    unordered_map<string, int> map;
+
+    for (int i = 0; i < want.size(); i++)
+        map.insert({want[i], number[i]});
+    
+    for (int i = 0; i < discount.size(); i++)
+    {
+        unordered_map<string, int> temp = map;
+        if(i+10 > discount.size())
+            break;
+        for (int j = i; j < i+10; j++)
+        {
+            if(temp.find(discount[j]) != temp.end())
+                temp[discount[j]]--;
+        }
+        int count{};
+        for(pair<string, int> iter : temp)
+        {
+            if(iter.second == 0)
+                count++;
+        }
+        if(count == want.size())
+            answer++;
+    }
+    
+
+    return answer;
+}
