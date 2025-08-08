0f4cad886521df1655f520a2d6490f2a5348de3b
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-08-08 10:18:11 +0900

    [Silver III] Title: NBA 농구, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.cc
+++ 백준/Silver/2852. NBA 농구/NBA 농구.cc
@@ -1,44 +1,46 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int n,o,A,B, aSum, bSum;
-string s, sPrev;
-
-string print(int a)
-{ 
-    string d = "00" + to_string(a / 60); 
-    string e = "00" + to_string(a % 60); 
-    return d.substr(d.size() - 2, 2) + ":" + e.substr(e.size() - 2, 2); 
+int toSec(string t) {
+    return stoi(t.substr(0, 2)) * 60 + stoi(t.substr(3, 2));
 }
 
-int ChangeToInt(string a)
-{
-    return atoi(a.substr(0, 2).c_str()) * 60 + atoi(a.substr(3, 2).c_str());
-}
-void Go(int& sum, string s)
-{
-    sum+= ChangeToInt(s) - ChangeToInt(sPrev);
+string toMMSS(int sec) {
+    char buf[6];
+    sprintf(buf, "%02d:%02d", sec / 60, sec % 60);
+    return string(buf);
 }
 
-int main()
-{
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        cin>> o >> s;
-        if(A>B)
-            Go(aSum, s);
-        else if(B>A)
-            Go(bSum, s);
-        o == 1 ? A++ : B++;
-        sPrev = s;
+int main() {
+    int N, score1 = 0, score2 = 0;
+    int time1 = 0, time2 = 0;
+    int leader = 0, leadStart = 0;
+    cin >> N;
+
+    for (int i = 0; i < N; ++i) {
+        int team;
+        string time;
+        cin >> team >> time;
+        int curTime = toSec(time);
+
+        if (team == 1) score1++;
+        else score2++;
+
+        int newLeader = 0;
+        if (score1 > score2) newLeader = 1;
+        else if (score2 > score1) newLeader = 2;
+
+        if (leader != newLeader) {
+            if (leader == 1) time1 += curTime - leadStart;
+            else if (leader == 2) time2 += curTime - leadStart;
+
+            leader = newLeader;
+            leadStart = curTime;
+        }
     }
 
-    if(A > B)
-    Go(aSum, "48:00");
-    else if(B > A)
-    Go(bSum, "48:00"); 
-    cout << print(aSum) << "\n"; 
-    cout << print(bSum) << "\n";
+    if (leader == 1) time1 += 2880 - leadStart;
+    else if (leader == 2) time2 += 2880 - leadStart;
+
+    cout << toMMSS(time1) << '\n' << toMMSS(time2) << '\n';
 }
