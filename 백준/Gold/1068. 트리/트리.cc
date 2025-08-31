d610d01c4b6c0cf49d57a32ef8df41198f448c80
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-08-31 22:04:58 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -1,47 +1,30 @@
-#include <iostream>
-#include <vector>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-int n{}, a{};
-int root{};
-vector<vector<int>> v(51);
-int leaf{};
-
-int DFS(int data)
-{
-	if (a == data)
-		return -1;
-	if (v[data].size() == 0)
-		return leaf++;
-
-	for (auto iter : v[data])
-	{
-		int temp = DFS(iter);
-		if (temp == -1 && v[data].size() == 1)
-			leaf++;
-	}
-
-	return leaf;
+int n, r, temp, root;
+vector<int> adj[54];
+int dfs(int here){
+    int ret = 0;
+    int child = 0;
+    for(int there : adj[here]){
+        if(there == r) continue;
+        ret += dfs(there);
+        child++;
+    }
+    if(child == 0) return 1;
+    return ret;
 }
-
-int main()
-{
-	cin >> n;
-
-	for (int i = 0; i < n; i++)
-	{
-		cin >> a;
-
-		if (a == -1)
-			root = i;
-		else
-			v[a].push_back(i);
-	}
-
-	cin >> a;
-
-	DFS(root);
-	cout << leaf;
-	
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> temp;
+        if(temp == -1)root = i;
+        else adj[temp].push_back(i);
+    }
+    cin >> r;
+    if(r == root){
+        cout << 0 << "\n";return 0;
+    }
+    cout << dfs(root) << "\n";
+    return 0;
 }
