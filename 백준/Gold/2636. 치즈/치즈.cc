5b2c2adc06919b3c166e253b79fbb596c7be9fdd
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-09-01 14:13:00 +0900

    [Gold IV] Title: 치즈, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.cc
+++ 백준/Gold/2636. 치즈/치즈.cc
@@ -1,68 +1,78 @@
 #include <iostream>
-#include <memory.h>
-using namespace std;
-int dx[4] = {0,-1,0,1}, dy[4] {-1,0,1,0};
+#include <vector>
 
-int n,m;
-int times{};
-int arr[100][100];
-int visited[100][100];
+using namespace std;
+
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,-1,0,1 };
+
+vector<vector<int>> board;
+vector<vector<bool>> visited;
+int n, m, times, ret;
+
+int Print()
+{
+	//cout << endl;
+	int count{};
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (visited[i][j] == true)
+				board[i][j] = 0;
+			if (board[i][j] == 1)
+				count++;
+			//cout << board[i][j] << " ";
+		}
+		//cout << endl;
+	}
+
+	//cout << count << endl;
+	return count;
+}
 
 void DFS(int x, int y)
 {
-    visited[x][y] = 1;
-    for (int i = 0; i < 4; i++)
-    {
-        int nx = x+dx[i];
-        int ny = y +dy[i];
-        if (nx < 0 || nx >= n || ny <0 || ny >=m)
-            continue;
-        if (visited[nx][ny] == 0)
-        {
-            if (arr[nx][ny] == 1)
-                visited[nx][ny] = 2;
-            else
-                DFS(nx,ny);
-        }
-    }
-}
+	visited[x][y] = true;
+	for (int i = 0; i < 4; i++)
+	{
+		int nx = dx[i] + x;
+		int ny = dy[i] + y;
 
-int Count()
-{
-    int count{};
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            if (visited[i][j] == 2)
-                arr[i][j] = 0;
-
-            if (arr[i][j] == 1)
-                count++;
-        }
-    }
-
-    return count;
+		if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+			continue;
+		if (visited[nx][ny] == false)
+		{
+			if (board[nx][ny] == 1)
+				visited[nx][ny] = true;
+			else
+				DFS(nx,ny);
+		}
+	}
 }
 
 int main()
 {
-    
-    cin>>n>>m;
-    for(int i=0;i<n;i++)
-        for(int j=0;j<m;j++)
-            cin>>arr[i][j];
-    int temp = Count();
-    while (true)
-    {
-        int count = Count();
-        if (count == 0)
-            break;
-        temp = count;
-        memset(visited,0,sizeof(visited));
-        DFS(0,0);
-        times++;
-    }
-    cout<<times<<endl;
-    cout<<temp<<endl;
+	cin >> n >> m;
+	board.assign(n, vector<int>(m, 0));
+	visited.assign(n, vector<bool>(m, 0));
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			cin >> board[i][j];
+		
+	int temp{};
+	while (true)
+	{
+		ret = temp;
+		temp = Print();
+		if (temp == 0)
+			break;
+		visited.assign(n, vector<bool>(m, false));
+		DFS(0,0);
+		times++;
+	}
+
+	cout << times << endl;
+	cout << ret << endl;
 }
