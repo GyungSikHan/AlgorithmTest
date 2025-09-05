978528cc8bddda75b9252d453dfb26cc767d4016
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-09-05 12:20:40 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 0 ms, Memory: 2908 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
@@ -1,59 +1,53 @@
+//https://www.acmicpc.net/problem/12869 v
+
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
 
 using namespace std;
 
-int n;
-int attack[6][3] = {
-	{9,3,1},
-	{9,1,3},
-	{3,9,1},
-	{3,1,9},
-	{1,9,3},
-	{1,3,9}
-};
-int scv[3];
-int visited[61][61][61];
-
-struct A {
-	int a, b, c;
-};
-queue<A>q;
-
-int Solve(int a, int b, int c)
+struct A { int a, b, c; };
+int hp[61][61][61];
+int attack[6][3]
 {
-	visited[a][b][c] = 1;
-	q.push({ a,b,c });
-	while (q.size())
-	{
-		int f = q.front().a;
-		int s = q.front().b;
-		int t = q.front().c;
-		q.pop();
-
-		if(visited[0][0][0] != 0)
-			break;
-		for (int i = 0; i < 6; i++)
-		{
-			int nF = max(0, f - attack[i][0]);
-			int nS = max(0, s - attack[i][1]);
-			int nT = max(0, t - attack[i][2]);
-			if (visited[nF][nS][nT] != 0)
-				continue;
-			visited[nF][nS][nT] = visited[f][s][t] +1;
-			q.push({ nF,nS,nT });
-		}
-	}
-
-	return visited[0][0][0] - 1;
-}
+	{9,3,1},{9,1,3},
+	{3,9,1}, {3,1,9},
+	{1,9,3}, {1,3,9}
+};
+int n, scv[3];
 
 int main()
 {
 	cin >> n;
 	for (int i = 0; i < n; i++)
 		cin >> scv[i];
-	cout << Solve(scv[0],scv[1],scv[2]) << endl;
+
+	queue<A> q;
+	q.push({ scv[0],scv[1],scv[2] });
+
+	while (q.empty() == false)
+	{
+		int a = q.front().a;
+		int b = q.front().b;
+		int c = q.front().c;
+		q.pop();
+		if (a == 0 && b == 0 && c == 0)
+		{
+			break;
+		}
+		for (int i = 0; i < 6; i++)
+		{
+			int na = max(a - attack[i][0],0);
+			int nb = max(b - attack[i][1],0);
+			int nc = max(c - attack[i][2],0);
+
+			if (hp[na][nb][nc] != 0)
+				continue;
+
+			hp[na][nb][nc] = hp[a][b][c] + 1;
+			q.push({ na,nb,nc });
+		}
+	}
+
+	cout << hp[0][0][0] << endl;
 }
