a1d6dd4a875d0b3937ed44ade6768ae833a8cfc1
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-09-05 13:15:43 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 0 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -1,4 +1,3 @@
-//https://www.acmicpc.net/problem/12851 v
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -6,49 +5,37 @@
 using namespace std;
 
 int n, k;
-
-int visited[100001];
+int arr[100001];
 int cnt[100001];
-int Count;
-
+queue<int> q;
 
 int main()
 {
 	cin >> n >> k;
-	if (n == k)
-	{
-		cout << 0 << endl;
-		cout << 1 << endl;
-		return 0;
-	}
 
-	queue<int> q;
 	q.push(n);
-	visited[n] = 1;
+	arr[n] = 1;
 	cnt[n] = 1;
+
 	while (q.empty() == false)
 	{
 		int curr = q.front();
 		q.pop();
-
-		if (curr == k)
-			break;
-
-		for (int next : {curr+1,curr-1, curr*2})
+		//cout << curr << endl;
+		for (int next : {curr -1, curr+1, curr*2})
 		{
 			if (next < 0 || next > 100000)
 				continue;
-			if (visited[next] == 0)
+			if (arr[next] == 0)
 			{
+				arr[next] = arr[curr] + 1;
+				cnt[next] += cnt[curr];
 				q.push(next);
-				visited[next] = visited[curr] + 1;
-				cnt[next]+= cnt[curr];
 			}
-			else if (visited[next] == visited[curr] + 1)
-				cnt[next]+= cnt[curr];
+			else if (arr[next] == arr[curr]+1)
+				cnt[next] += cnt[curr];
 		}
 	}
-
-	cout << visited[k]-1 << endl;
+	cout << arr[k]-1 << endl;
 	cout << cnt[k] << endl;
 }
