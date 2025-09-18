8da6f46285f420ac4b29dc154635a6754dd2e8bb
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-09-18 13:44:51 +0900

    [Silver V] Title: 집합, Time: 544 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.cc
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+const int Max = 21;
+
+int m, k;
+string str;
+set<int> s;
+int arr[Max];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	cin >> m;
+
+	for (int i = 0; i < m; i++)
+	{
+		cin >> str;
+		if (str == "all")
+			fill(arr,arr+Max, 1);
+		else if (str[0] == 'e')
+			fill(arr, arr+Max, 0);
+		else
+		{
+			cin >> k;
+			if (str[0] == 'a')
+				arr[k] = 1;
+			else if (str[0] == 'r')
+			{
+				if (arr[k] == 1)
+					arr[k] = 0;
+			}
+			else if (str[0] == 'c')
+			{
+				if (arr[k] == 1)
+					cout << 1 << "\n";
+				else
+					cout << 0 << "\n";
+			}
+			else if (str[0] == 't')
+			{
+				if (arr[k] == 1)
+					arr[k] = 0;
+				else
+					arr[k] = 1;
+			}
+		}
+	}
+}
