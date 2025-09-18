85c214acf95d298ec3ed2dc3eded749bf68f3a49
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-09-18 14:40:47 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.cc
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -1,46 +1,14 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-int x, stick = 64, cnt;
-
-int main()
-{
-	cin >> x;
-
-	if (x == 64)
-	{
-		cout << 1;
-		return 0;
+int n, ret = 1;
+int main(){ 
+	cin >> n; 
+	while(n != 1){
+        if(n & 1) ret++; 
+        n /= 2;
 	}
-
-	vector<int> v;
-	v.push_back(stick);
-	while (true)
-	{
-		int half = v.back();
-		if (x == half)
-			break;
-
-		v.pop_back();
-		half /= 2;
-		int sum = half;
-		for (int i : v)
-			sum += i;
-
-		if (sum >= x)
-		{
-			v.push_back(half);
-			if (sum == x)
-				break;
-		}
-		else
-		{
-			v.push_back(half);
-			v.push_back(half);
-		}
-	}
-
-	cout << v.size();
+	cout << ret << '\n';
+    return 0;
 }
