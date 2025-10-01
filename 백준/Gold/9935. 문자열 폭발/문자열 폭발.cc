bbc3e06d601e4c62ddf3f6f6de818fff7a8ae219
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-10-01 15:17:13 +0900

    [Gold IV] Title: 문자열 폭발, Time: 48 ms, Memory: 8756 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -1,27 +1,47 @@
 #include <iostream>
+#include <stack>
 #include <vector>
 #include <string>
-
+#include <algorithm>
 using namespace std;
 
 string s;
 string boom;
+stack<char> st;
 
 int main()
 {
 	cin >> s;
 	cin >> boom;
-
-	string ret{};
 	for (char c : s)
 	{
-		ret += c;
-		if (ret.size() >= boom.size() && ret.substr(ret.size() - boom.size(), boom.size()) == boom)
-			ret.erase(ret.size() - boom.size(), ret.size());
+		st.push(c);
+		if (st.size() >= boom.size() && st.top() == boom[boom.size() - 1])
+		{
+			string temp{};
+			for (int i = 0; i < boom.size(); i++)
+			{
+				temp += st.top();
+				st.pop();
+			}
+			reverse(temp.begin(),temp.end());
+			if (temp != boom)
+				for (char iter : temp)
+					st.push(iter);
+		}
 	}
 
-	if (ret == "")
+	if (st.empty() == true)
 		cout << "FRULA";
 	else
-		cout << ret;
+	{
+		string temp{};
+		while (st.empty() == false)
+		{
+			temp += st.top();
+			st.pop();
+		}
+		reverse(temp.begin(), temp.end());
+		cout << temp;
+	}
 }
