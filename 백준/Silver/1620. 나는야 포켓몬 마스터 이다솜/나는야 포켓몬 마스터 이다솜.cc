8a88c6d92460180ec3e4f29a8220f6f6516095eb
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-12-02 14:08:55 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 184 ms, Memory: 23944 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,35 +1,37 @@
-#include <iostream>
-#include <map>
-#include <string>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-int n, m;
-map<string, int> m1;
-map<int, string> m2;
-string s;
+int n,m;
+map<int, string> ma;
+map<string, int> ma2;
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+    cin>>n>>m;
+    string s;
+    for(int i = 1; i <= n; i++)
+    {
+        cin>>s;
+        ma.insert({i,s});
+        ma2.insert({s,i});
+    }
     
-	cin >> n >> m;
+    for(int i = 0; i < m; i++)
+    {
+        cin>>s;
+        if(s[0] >='A' && s[0] <='Z')
+        {
+            cout<<ma2[s]<<"\n";
+        }
+        else
+        {
+            cout<<ma[stoi(s)]<<"\n";
+        }
+    }
 
-	for (int i = 1; i <= n; i++)
-	{
-		cin >> s;
-		m1.insert({ s,i });
-		m2.insert({ i,s });
-	}
-
-	for (int i = 0; i < m; i++)
-	{
-		cin >> s;
-		if (atoi(s.c_str()) == 0)
-			cout << m1[s] << "\n";
-		else
-			cout << m2[atoi(s.c_str())] << "\n";
-	}
+    
 }
