8728ead0522fb0f96223fc18db849e322cccfcd0
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-12-07 14:25:21 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,52 +1,60 @@
-#include <iostream>
+#include <cstdio>
+#include<iostream>
 #include <vector>
 #include <queue>
+
 using namespace std;
 
-int dx[4] = { -1,0,1,0 };
-int dy[4] = { 0,-1,0,1 };
-int n, m;
-queue<pair<int, int>> q;
-vector<vector<int>> v;
+int dy[4] = {-1,0,1,0};
+int dx[4] ={0,-1,0,1};
+int n{},m{};
+vector<vector<int>> board;
 vector<vector<int>> visited;
 
-void BFS(int y, int x)
-{
-	q.push({y, x});
-	visited[y][x] = 1;
+void Solution(int x, int y)
+{   
+    visited[x][y] = 1;
+    queue<pair<int,int>>q;
+    q.push({x,y});
 
-	while (q.empty() == false)
-	{
-		int curry = q.front().first;
-		int currx = q.front().second;
-		q.pop();
-		
-		for (int i = 0; i < 4; i++)
-		{
-			int ny = curry + dy[i];
-			int nx = currx + dx[i];
-			if(ny >=0 && ny < n && nx >= 0 && nx < m)
-			{
-				if(visited[ny][nx] == false && v[ny][nx] == 1)
-				{
-					visited[ny][nx] = visited[curry][currx] + 1;
-					q.push({ ny,nx });
-				}
-			}
-		}
-	}
+    while(q.empty() == false)
+    {
+        int currx = q.front().first;
+        int curry = q.front().second;
+        q.pop();
+
+        for(int i = 0; i<4; i++)
+        {
+            int nx = currx+dx[i];
+            int ny = curry+dy[i];
+
+            if(nx<0|| nx>=n||ny<0||ny>=m)
+                continue;
+            if(visited[nx][ny] != 0 || board[nx][ny] == 0)
+                continue;
+            visited[nx][ny] = visited[currx][curry]+1;
+            q.push({nx,ny});
+        }
+    }
 }
 
 int main()
 {
-	cin >> n >> m;
-	v.resize(n, vector<int>(m, 0));
-	visited.resize(n, vector<int>(m, 0));
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			scanf("%1d", &v[i][j]);
 
+    cin>>n>>m;
+    board.resize(n, vector<int>(m, 0));
+    visited.resize(n,vector<int>(m,false));
+    for (int i = 0; i < n; i++) 
+        for (int j = 0; j < m; j++) 
+            scanf("%1d",&board[i][j]);
+        
+    if(board[0][0] == 0)
+    {
+        cout<<0<<endl;
+        return 0;
+    }
 
-	BFS(0,0);
-	cout << visited[n-1][m-1] << endl;
+    Solution(0, 0);
+
+    cout<<visited[n-1][m-1]<<endl;
 }
