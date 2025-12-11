7f2b65767ad3cbf293286936b7522563f72f7140
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-12-12 03:41:04 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -1,30 +1,43 @@
 #include<bits/stdc++.h>
+
 using namespace std;
-int n, r, temp, root;
-vector<int> adj[54];
-int dfs(int here){
-    int ret = 0;
-    int child = 0;
-    for(int there : adj[here]){
-        if(there == r) continue;
-        ret += dfs(there);
-        child++;
+int n, a, d, root, ret;
+vector<vector<int>> v;
+
+int DFS(int x)
+{
+    int temp{};
+    
+    for(auto iter : v[x])
+    {
+        if( iter != d)
+            temp += DFS(iter);
     }
-    if(child == 0) return 1;
-    return ret;
+    if(temp == 0)
+    {
+        return 1;
+    }
+
+    return temp;
 }
-int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> n;
-    for(int i = 0; i < n; i++){
-        cin >> temp;
-        if(temp == -1)root = i;
-        else adj[temp].push_back(i);
+
+int main()
+{
+    cin>>n;
+    v.resize(n);
+        for(int i = 0; i < n; i++)
+    {
+        cin>>a;
+        if(a == -1)
+            root = i;
+        else
+        {
+            v[a].push_back(i);
+        }
     }
-    cin >> r;
-    if(r == root){
-        cout << 0 << "\n";return 0;
-    }
-    cout << dfs(root) << "\n";
-    return 0;
+
+    cin>>d;
+    if(root != d )
+        ret = DFS(root);
+    cout<<ret;
 }
