d220411cf34b77c07a0229cb49cc58bb809c2f6e
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-12-14 14:14:53 +0900

    [Silver III] Title: 교수가 된 현우, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -1,27 +1,27 @@
-#include <iostream>
-#include <algorithm>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-int T;
-int N;
+int t;
 
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    cin>>T;
-    for (int i = 0; i < T; i++)
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cin>>t;
+    while(t--)
     {
-        cin>>N;
-        int ret{},ret2{};
-        for (int j = 2; j <= N; j*=2)
-            ret += N/j;
-        for (int j = 5; j <= N; j*=5)
-            ret2 += N/j;
-        
-        cout<<min(ret, ret2)<<"\n";
+        int a{};
+        int ret{};
+        int temp = 1;
+        cin>>a;
+
+        for(int i = 5; i <= a; i*=5)
+        {
+            ret += (a/i);
+        }
+
+        cout<<ret<<"\n";
     }
-    
 }
