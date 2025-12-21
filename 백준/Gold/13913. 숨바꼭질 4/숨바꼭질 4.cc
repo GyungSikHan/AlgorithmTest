240370d4c6f29bb8583cc59e68b22c6380eb97ae
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-12-21 11:10:44 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 12 ms, Memory: 4368 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -1,58 +1,35 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-int MAX = 200002;
-int n, k;
-vector<int> Time, Node, Result;
-queue<int> q;
-
-void InpudData()
-{
-	int a = k;
-	while (a > -1 )
-	{
-		Result.push_back(a);
-		a = Node[a];
-	}
-}
-
-int main()
-{
-	cin >> n >> k;
-	Time.resize(MAX+1, 0);
-	Node.resize(MAX+1, -1);
-
-	q.push(n);
-	Time[n] = 1;
-
-	while (q.empty() == false)
-	{
-		int curr = q.front();
-		q.pop();
-		
-		for (int next : {curr + 1, curr - 1, curr * 2})
-		{
-			if(next < 0 || next > MAX)
-				continue;
-
-			if(Time[next] == 0)
-			{
-				Time[next] = Time[curr] + 1;
-				Node[next] = curr;
-				q.push(next);
-			}
-		}
-	}
-	InpudData();
-	
-
-	cout << Time[k] - 1 << endl;
-	for (int i = Result.size()-1; i >= 0; i--)
-		cout << Result[i] << " ";
-	cout << endl;
-
+#define prev aaa
+#define next aaaa
+const int max_n = 200004;
+int visited[max_n], prev[max_n], n, k, ret, here, cnt, next;   
+vector<int> v; 
+queue<int> q;  
+int main() { 
+    cin >> n >> k; 
+    visited[n] = 1; 
+    q.push(n);  
+    while(q.size()){
+        here = q.front();  
+        q.pop();
+        if(here == k){
+            ret = visited[k]; 
+            break;
+        }
+		for(int next : {here + 1, here - 1, here * 2}){
+            if(next >= max_n || next < 0 || visited[next]) continue;  
+            visited[next] = visited[here] + 1; 
+            prev[next] = here; 
+            q.push(next); 
+		} 
+    }   
+    for(int i = k; i != n; i = prev[i]){
+        v.push_back(i);
+    }
+    v.push_back(n);
+    cout << ret - 1<< '\n'; 
+    reverse(v.begin(), v.end());
+	for(int i : v) cout << i << ' '; 
+    return 0;
 }
