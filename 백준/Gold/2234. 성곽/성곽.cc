b57bf47df4f48b0da1a635fcc872001d80eaea62
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2025-12-28 03:44:54 +0900

    [Gold III] Title: 성곽, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/2234. 성곽/성곽.cc
+++ 백준/Gold/2234. 성곽/성곽.cc
@@ -1,80 +1,124 @@
-//https://www.acmicpc.net/problem/2234
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
-
-int dx[4]{ 0,-1,0,1 }, dy[4]{ -1,0,1,0 };
-
-int n, m;
-int arr[51][51];
-int visited[51][51];
-int pa[51];
-int cnt, h = -1, sum;
-
-void DFS(int x, int y, int num)
+int dy[4]{0,-1,0,1}, dx[4]{-1,0,1,0};
+int n, m, ret1, ret2 = -1, ret3 = -1;
+vector<vector<int>> ma;
+vector<vector<int>> visited;
+vector<vector<int>>visited2;
+vector<int> v;
+vector<int> v2;
+void Print()
 {
-	if (visited[x][y] != 0)
-		return;
-	h++;
-	visited[x][y] = num;
-	for (int i = 0; i < 4; i++)
-	{
-		if ((arr[x][y] & 1<<i) == 0)
-		{
-			int nx = dx[i] + x;
-			int ny = dy[i] + y;
-			DFS(nx,ny,num);
-		}
-	}
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << visited[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int DFS(int y, int x, const int& team)
+{
+    visited[y][x] = team;
+    //Print();
+    int sum = 1;
+    for(int i = 0; i<4 ; i++)
+    {
+        if((ma[y][x] & (1<<i)) == 0)
+        {
+            int ny = y+dy[i];
+            int nx = x+dx[i];
+            if(visited[ny][nx] == 0)
+                sum += DFS(ny,nx, team);
+        }
+    }
+
+    return sum;
+}
+
+void BFS(int y, int x)
+{
+    queue<pair<int, int>>qu;
+    qu.push({y,x});
+    visited2[y][x] = 1;
+    vector<int> temp = v;
+    while(qu.empty() == false)
+    {
+        int curry = qu.front().first;
+        int cyrrx = qu.front().second;
+        qu.pop();
+        for(int i = 0; i<4; i++)
+        {
+            int ny = curry+dy[i];
+            int nx = cyrrx+dx[i];
+            if(ny<0||m <= ny || nx<0||n<= nx)
+                continue;
+            if(visited2[ny][nx] == 1)
+                continue;
+            if(visited[ny][nx] == visited[y][x])
+            {
+                qu.push({ny,nx});
+                visited2[ny][nx] = 1;
+            }
+            else
+            {
+                if(temp[visited[ny][nx]] == 1)
+                    continue;
+
+                temp[visited[ny][nx]] = 1;
+                ret3 = max(ret3, v2[visited[y][x]-1] + v2[visited[ny][nx]-1]);
+            }
+        }
+    }
 }
 
 int main()
 {
-	cin >> n >> m;
+    cin >> n >> m;
+    ma.resize(m, vector<int>(n, 0));
+    visited.resize(m, vector<int>(n, 0));
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> ma[i][j];
+        }
+    }
 
-	for (int i = 0; i < m; i++)
-		for (int j = 0; j < n; j++)
-			cin >> arr[i][j];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (visited[i][j] == 0)
+            {
+                int temp{};
+                ret1++;
+                //cout<<ret1<<endl;
+                temp = DFS(i, j, ret1);
+                v2.push_back(temp);
+                ret2 = max(ret2, temp);
+            }
+        }
+    }
 
-	int num = 1;
-	
-	for (int i = 0; i < m; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			if (visited[i][j] == 0)
-			{
-				h = 0;
-				DFS(i,j,num);
-				pa[num] = h;
-				cnt = max(cnt,h);
-				num += 1;
-			}
-		}
-	}
-
-	for (int i = 0; i < m; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			if (i+1 < m)
-			{
-				int a = visited[i][j];
-				int b = visited[i + 1][j];
-				if (a != b)
-					sum = max(sum, pa[a] + pa[b]);
-			}
-			if (j + 1 < n)
-			{
-				int a = visited[i][j];
-				int b = visited[i][j+1];
-				if (a != b)
-					sum = max(sum, pa[a] + pa[b]);
-			}
-		}
-	}
-	cout << num-1 << "\n";
-	cout << cnt << "\n";
-	cout << sum;
+    v.resize(ret1+1, 0);
+    visited2.resize(m,vector<int>(n, 0));
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j<n; j++)
+        {
+            if(visited2[i][j] == 0)
+            {
+                v[visited[i][j]] = 1;
+                BFS(i,j);
+            }
+        }
+    }
+    cout << ret1 << "\n";
+    cout << ret2 << "\n";
+    cout <<ret3<<"\n";
 }
