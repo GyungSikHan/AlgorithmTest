0a608240c778878995576a3621a90d4a7d060d84
GyungSikHan <165019291+GyungSikHan@users.noreply.github.com>
2026-01-05 20:06:58 +0900

    [Gold IV] Title: 문자열 폭발, Time: 16 ms, Memory: 7000 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -1,55 +1,69 @@
-#include <iostream>
-#include <queue>
-#include <vector>
-#include <string>
-#include <stack>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-string s{};
-string boom{};
+string s, boom;
+stack<char> st;
+string ret;
+
 int main()
 {
-	cin >> s;
-	cin >> boom;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-	stack<char> st;
+    cin >> s;
+    cin >> boom;
+    for(int i = 0; i < s.size(); i ++)
+    {
+        st.push(s[i]);
+        if(st.size() >= boom.size() && st.top() == boom[boom.size() - 1])
+        {
+            string temp{};
+            for(int j = 0; j < boom.size(); j++)
+            {
+                temp += st.top();
+                st.pop();
+            }
+            reverse(temp.begin(), temp.end());
 
-	for (int i = 0; i < s.size(); i++)
-	{
-		st.push(s[i]);
+            if(temp != boom)
+            {
+                for(int j = 0; j < temp.size(); j++)
+                    st.push(temp[j]);
+            }
+        }
+    }
+    // for (auto iter = s.rbegin(); iter != s.rend(); iter++)
+    // {
+    //     st.push(*iter);
+    //     if (st.size() >= boom.size() && st.top() == boom[0])
+    //     {
+    //         string temp{};
+    //         for (int i = 0; i < boom.size(); i++)
+    //         {
+    //             temp += st.top();
+    //             st.pop();
+    //         }
+    //         cout<<temp<<endl;
+    //         if(temp != boom)
+    //         {
+    //             for(int i = 0; i < temp.size(); i++)
+    //                 st.push(temp[i]);
+    //         }
+    //     }
+    // }
 
-		if (st.size() >= boom.size() && st.top() == boom[boom.size()-1])
-		{
-			string temp{};
-			for (int j = 0; j < boom.size(); j++)
-			{
-				temp += st.top();
-				st.pop();
-			}
-
-			reverse(temp.begin(), temp.end());
-
-			if (temp != boom)
-				for (int j = 0; j < temp.size(); j++)
-					st.push(temp[j]);
-		}
-	}
-
-	string ret{  };
-
-	if (st.empty() == true)
-		cout << "FRULA";
-	else
-	{
-		while (st.empty() == false)
-		{
-			ret += st.top();
-			st.pop();
-		}
-		reverse(ret.begin(),ret.end());
-
-		cout << ret;
-	}
+    if (st.empty() == true)
+        cout << "FRULA";
+    else
+    {
+        while (st.empty() == false)
+        {
+            ret += st.top();
+            st.pop();
+        }
+        reverse(ret.begin(), ret.end());
+        cout << ret;
+    }
 }
